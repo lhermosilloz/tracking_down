b1ee0e8d9db92160bf5ce98703f2b08516bedb38
leonciohermosillo <lhermosillo@pheratech.com>
2026-03-17 15:40:25 -0700

    setup steps for precision landing

---
+++ src/precision_land/launch/precision_landing.launch.py
@@ -0,0 +1,8 @@
+from launch import LaunchDescription
+from launch_ros.actions import NOde
+from launch_ros.substitutions import FindPackageShare
+from launch.substitutions import PathJoinSubstitution, LaunchConfiguration
+
+def generate_launch_desctiption():
+    # Declare launch args to enable vis
+    pass
